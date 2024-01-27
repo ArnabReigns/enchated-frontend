c0575adfb2b0394a67e72d0c5d11dd75ea7f8485
Arnab Chatterjee <arnabchatterjee912@gmail.com>
2024-01-28 02:03:05 +0530

    update

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-console.log(import.meta.env.VITE_BACKEND_URL)
+console.log(import.meta.env.VITE_BACKEND_URL);
 const api = axios.create({
   baseURL: import.meta.env.PROD
-    ? "https://"
+    ? "https://" + import.meta.env.VITE_BACKEND_URL
     : "http://" + import.meta.env.VITE_BACKEND_URL,
 });
 
